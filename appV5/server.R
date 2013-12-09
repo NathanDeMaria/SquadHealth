12f40db87af63619691c56d4ebaf5d496a5ca689
NathanDeMaria <nathan.demaria@hudl.com>
2013-12-09 10:39:41 -0600

    Added appV5

---
+++ appV5/server.R
@@ -0,0 +1,98 @@
+library(shiny)
+
+# Define server logic required to generate and plot a random distribution
+shinyServer(function(input, output) {
+  
+  time.period = reactive({input$time.period})
+  
+  num.cols = reactive({length(unique(data$squad[data$quarterId == input$time.period]))})
+  
+  output$style = renderText({
+    paste0('
+<style type="text/css">
+  .first {
+    height:50px; 
+    display:inline-block; 
+    margin-right:10px;
+    margin-top:10px;
+    text-align:center;
+    vertical-align:middle;
+    padding-top:16px;   
+    box-sizing: border-box;   
+    width:15%;
+  }
+  .box {
+    height:50px; 
+    border-radius:5px; 
+    display:inline-block; 
+    margin-right:10px;
+    margin-top:10px;
+    text-align:center;
+    vertical-align:middle;
+    padding-top:16px;   
+    box-sizing: border-box;
+    width:', min(70 / num.cols(), 15), '%
+  }
+  .white {
+    background-color:white;
+    color:black;
+  }
+  .yellow {
+    background-color:#FFFFBF;
+    color:black;
+  }
+  .green {
+    background-color:#1A9641;
+    color:black;
+  }
+  .red {
+    background-color:#D7191C;
+    color:white;
+  }
+  .yellowGreen {
+    background-color:#A6D96A;
+    color:black;
+  }
+  .orange {
+    background-color:#FDAE61;
+    color:black;
+  }
+</style>               
+                  ')
+  })
+  
+  output$table = renderText({
+  
+    # calculates ratings for this quarter
+    ratings = sapply(levels(data$squad[data$quarterId == time.period()]), function(x) {get.avg(x, time.period())})
+
+    # choices is now in order, so this actually does represent the previous quarter's index
+    prev.time = which(choices==time.period())-1
+    
+    # if the previous quarter doesn't exist, then it says that prevRatings are the current ratings
+      # so there will all be marked with -'s
+    if(prev.time > 0)
+    {
+      prevRatings = sapply(levels(data$squad), function(x) {get.avg(x, choices[prev.time] )})
+    }
+    else
+    {
+      prevRatings=ratings
+    }
+    
+    # hard coded in because the XML just says Field 4, etc.
+      # I think if you want to change these things, you might have to change my R code here and in global.R
+    rownames(ratings) = c('Tribe Leader', 'PM', 'Influence Work', 'Easy Release', 'Process', 'Mission', 'Take Risks', 'Org. Support')
+
+    # to handle adding/removing squads
+    valid.cols = which(colnames(ratings) %in% colnames(ratings)[!is.na(ratings[1,])])
+    
+    headrow = paste0('<tr><th class="first"></th>', paste0(lapply(colnames(ratings)[valid.cols], function(name) {paste0('<th class="box">', name, '</th>',collapse='')}), collapse=''), '</tr>', collapse='')
+    
+    rows = paste0(lapply(1:dim(ratings)[1], function(rowNum) {make.row(rownames(ratings)[rowNum], ratings[rowNum,valid.cols], prevRatings[rowNum,valid.cols])}),collapse='')
+      
+    # switch from 100% to 1600px avoid getting gross if table can't fit
+    paste0('<table width=1600px><thead>', headrow, '</thead><tbody>', rows, '</tbody></table>')
+  })
+
+})
